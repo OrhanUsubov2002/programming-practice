4fb5333ff9164ac24e293c7e776155f6a65140e9
Orhan Usubov <klakolvlad@gmail.com>
2021-08-03 19:55:28 +0300

    logic Operations Task

--- first-task/script.js
+++ first-task/script.js
@@ -1,1 +1,23 @@
-//hello world
+"use strict"; 
+
+const LOGIN = "Админ"; 
+const PASSWORD_FOR_LOGIN = "Я Главный"; 
+
+const currentUser = prompt("Кто?"); 
+
+if (currentUser === LOGIN) {
+  const passwordUSer = prompt("Введите пароль"); 
+  
+  if (passwordUSer === PASSWORD_FOR_LOGIN) {
+    alert("Здравствуйте"); 
+  } else if (passwordUSer == null || passwordUSer == "") {
+    alert("Отменено"); 
+  } else {
+    alert("Я вас не знаю"); 
+  }
+} else if (currentUser == null || currentUser == "" ) {
+  alert("Отменено"); 
+} else { 
+  alert("Я вас не знаю"); 
+}
+  
